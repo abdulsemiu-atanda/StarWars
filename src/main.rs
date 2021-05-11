a9a35f9f2f365cd73273e48b27ba53e1a2618bac
Abdulsemiu Atanda <semiu@live.com>
2021-05-11 15:52:20 -0400

    make cli more interactive

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,58 @@
-mod api;
+use console::style;
+use dialoguer::{theme::ColorfulTheme, Select};
+use loading::Loading;
+use std::thread;
+use std::time::Duration;
+use std::collections::HashMap;
 
-use api::api_tools::*;
-use api::models::*;
+mod api;
+mod handlers;
+
+use handlers::user_request::*;
 
 fn main() {
-    let endpoint = api_url("people");
-    let response = api_response(&endpoint);
-    let data: ApiResponse = response.json().unwrap();
+    let mut hash = HashMap::new();
 
-    println!("Hello, world!");
+    // setup hash to match to shorthand values
+    hash.insert("People".to_string(), "P".to_string());
+    hash.insert("Starships".to_string(), "S".to_string());
+    hash.insert("Films".to_string(), "F".to_string());
 
-    for item in &data.results {
-        println!("{}", item.summarize());
+    let selections = &["People", "Starships", "Films"];
+
+    println!("{}", style("Welcome Star Wars!").cyan());
+
+    let selection = Select::with_theme(&ColorfulTheme::default())
+        .with_prompt("Please select one of these options to get started")
+        .default(0)
+        .items(&selections[..])
+        .interact()
+        .unwrap();
+
+    let response = String::from(selections[selection]);
+    let input = hash.get(&response).unwrap();
+
+    // setup loader
+    let mut loading = Loading::new();
+
+    loading.start();
+
+    for i in 0..20 {
+        let string = if i % 2 == 0 { String::from("..") } else { String::from("...") };
+
+        loading.text(format!("Loading{}", string));
+        thread::sleep(Duration::from_millis(50));
     }
+
+    let data = resource_summaries(&input);
+
+    loading.success("OK");
+    println!();
+    println!("{}", "-".repeat(120));
+    loading.end();
+
+    for item in &data {
+        println!("{}", item);
+    }
+    println!("{}", "-".repeat(120));
 }
